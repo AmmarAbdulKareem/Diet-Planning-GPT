283e2e253d50523322e36a3d16c0ababb3a5a8db
AmmarAbdulKareem <98031063+AmmarAbdulKareem@users.noreply.github.com>
2023-05-03 23:07:03 +0300

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,12 @@
+let btnResult=document.getElementById("btn1");
+let c1=document.getElementById("calories");
+let r11=document.getElementById("r1");
+
+
+
+function meals (){
+
+  if(c1.value>2000){
+    r11.style.display='block';
+  }
+}
